bfa6b41b628ab1fa6bebc241f1365380b4d5c7ee
Christiaan Mealey <christiaan.mealey@altdriver.com>
2015-10-12 11:33:21 -0400

    gulp default tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,70 @@
 'use strict';
 
-var gulp = require('gulp'),
+var gulp            = require('gulp'),
     gulpLoadPlugins = require('gulp-load-plugins'),
     sass            = require('gulp-sass'),
+    git             = require('gulp-git'),
+    fs              = require('fs'),
+    es              = require('event-stream'),
     path            = require('path'),
+    knox            = require('knox'),
+    clean           = require('gulp-clean'),
     browserify      = require('gulp-browserify'),
     runSequence     = require('run-sequence'),
+    pkg             = require('./package.json'),
     plugins         = gulpLoadPlugins(),
     csslint         = require('gulp-csslint'),
-    cssmin          = require('gulp-cssmin');
-
-var env = process.env.NODE_ENV || 'development';
-
-// read gulp directory contents for the tasks...
-require('require-dir')('./gulp');
+    cssmin          = require('gulp-cssmin'),
+    jasmine         = require('gulp-jasmine'),
+    reporters       = require('jasmine-reporters'),
+    requireDir      = require('require-dir'),
+    Server          = require('karma').Server;
 
 var paths   = {
-    js: ['../app/**/*.js', '!tests/**/*.js'],
-    sass: ['../assets/**/*.scss'],
-    assets:['../assets/**/*.*', '!assets/**/*.scss'],
-    templates: ['../app/components/**/*.html'],
-    tests: ['../tests/spec/**/*.js'],
-    config: ['../app/config/*.json']
+    js: ['app/**/*.js', '!tests/**/*.js'],
+    sass: ['assets/**/*.scss'],
+    assets:['assets/**/*.*', '!assets/**/*.scss'],
+    templates: ['app/components/**/*.html'],
+    tests: ['tests/spec/**/*.js'],
+    config: ['app/config/*.json']
 };
 
 gulp.task('scripts', ['lint'], function(){
-    return gulp.src('/app/app.js')
+    return gulp.src('app/app.js')
         .pipe(browserify({
             insertGlobals: true
         }))
-        .pipe(gulp.dest('../dist/js'))
+        .pipe(gulp.dest('./dist/js'))
 });
 
 gulp.task('templates', function(){
     gulp.src(paths.templates)
-        .pipe(gulp.dest('../dist/'));
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('config', function(){
     gulp.src(paths.config)
-        .pipe(gulp.dest('../dist/appdata/'));
+        .pipe(gulp.dest('./dist/appdata/'));
 });
 
 gulp.task('data', function(){
     gulp.src(paths.config)
-        .pipe(gulp.dest('../data/'));
+        .pipe(gulp.dest('./data/'));
+});
+
+gulp.task('test', function (done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done).start();
+});
+
+gulp.task('tests', function(){
+    gulp.src('app/app.mock.js')
+        .pipe(browserify({
+            insertGlobals: true
+        }))
+        .pipe(gulp.dest('./tests/src'))
 });
 
 gulp.task('lint', function() {
@@ -56,20 +76,20 @@ gulp.task('lint', function() {
 
 gulp.task('assets', function() {
     return gulp.src(paths.assets)
-        .pipe(gulp.dest('../dist/'));
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('css',['css:min']);
 
 gulp.task('css:min', function() {
-    return gulp.src('../dist/css/styles.css')
+    return gulp.src('./dist/css/styles.css')
         .pipe(cssmin())
         .pipe(plugins.rename({suffix: '.min'}))
-        .pipe(gulp.dest('../dist/css'));
+        .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('css:lint', function(){
-    gulp.src('../dist/css/*.css')
+    gulp.src('./dist/css/*.css')
         .pipe(csslint())
         .pipe(csslint.reporter());
 });
@@ -77,9 +97,42 @@ gulp.task('css:lint', function(){
 gulp.task('css:sass', function () {
     return gulp.src(paths.sass)
         .pipe(sass({
-            paths: [ '../assets/css/' ]
+            paths: [ './assets/css/' ]
         }))
-        .pipe(gulp.dest('../dist'));
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('clean', function () {
+    return gulp.src('./dist', { read: false })
+        .pipe(clean());
+});
+
+gulp.task('env:development', function () {
+    process.env.NODE_ENV = 'development';
+});
+
+gulp.task('env:production', function () {
+    process.env.NODE_ENV = 'production';
+});
+
+gulp.task('devServe', ['env:development'], function () {
+    plugins.nodemon({
+        script: 'server.js',
+        ext: 'html js',
+        env: { 'NODE_ENV': 'development' } ,
+        ignore: ['node_modules/', 'bower_components/', 'logs/', 'packages/*/*/public/assets/lib/', 'packages/*/*/node_modules/', '.DS_Store', '**/.DS_Store', '.bower-*', '**/.bower-*'],
+        nodeArgs: ['--debug'],
+        stdout: false
+    }).on('readable', function() {
+        this.stdout.on('data', function(chunk) {
+            if(/app listening/.test(chunk)) {
+
+                //setTimeout(function(){ plugins.livereload.reload(); }, 500);
+            }
+            process.stdout.write(chunk);
+        });
+        this.stderr.pipe(process.stderr);
+    });
 });
 
 gulp.task('watch', function () {
@@ -91,11 +144,8 @@ gulp.task('watch', function () {
     gulp.watch(paths.tests, ['tests']);
 });
 
+gulp.task('default',['build','devServe','watch']);
+
 gulp.task('build', function(callback) {
     runSequence('clean', 'css:sass', 'css', 'assets', 'templates', 'config', 'data', 'scripts', callback);
-});
-
-gulp.task('default', function () {
-    // run with paramater
-    gulp.start(env);
 });
