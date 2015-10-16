cf5c31060db0ebef3393814bac09219a064147a7
Christiaan Mealey <christiaan.mealey@altdriver.com>
2015-10-16 14:00:19 -0400

    fb routes

--- server.js
+++ server.js
@@ -351,7 +351,7 @@ app.get('/:category/:slug', function(req,res, next){
                 metatags.fb_url = post.link;
                 metatags.fb_image = post.featured_image_src.medium[0];
 
-                res.send('<html><head><meta property="og:locale" content="en_US"><meta property="og:url" content="'+ metatags.fb_url + '" ><meta property="og:title" content="'+ metatags.fb_title +'" ><meta property="og:image" content="'+ metatags.fb_image +'" ><meta property="og:description" content="'+ metatags.fb_description +'" ><meta property="og:site_name" content="http://driversenvy.com" ><meta property="og:type" content="'+ metatags.fb_type +'" ><meta property="fb:app_id" content="638692042912150"></head><body></body></html>');
+                res.send('<html><head><meta property="og:locale" content="en_US"><meta property="og:title" content="'+ metatags.fb_title +'" ><meta property="og:image" content="'+ metatags.fb_image +'" ><meta property="og:description" content="'+ metatags.fb_description +'" ><meta property="og:site_name" content="http://driversenvy.com" ><meta property="og:type" content="'+ metatags.fb_type +'" ><meta property="fb:app_id" content="638692042912150"></head><body></body></html>');
             }
 
         });
