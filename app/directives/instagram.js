7fc480ce4ea2d071e615804c72987f21ff2d64a8
Christiaan Mealey <christiaan.mealey@altdriver.com>
2015-11-10 13:12:05 -0500

    instagram fix

--- app/directives/instagram.js
+++ app/directives/instagram.js
@@ -4,13 +4,10 @@ var instagram = function() {
     return {
         restrict: 'EA',
         controller: function($scope, $attrs, InstagramService) {
-            $scope.gram = {};
-            var maxId = $attrs.maxId;
-            return InstagramService.get(1, 'nofilter', maxId).then(function(res){
-                $scope.gram = res.data;
-                return $scope.gram;
-            });
-        }, template: '<a href="{{ gram.data.link }}" target="_blank"><img class="instagram-img" src="{{ gram.data.images.standard_resolution.url }}"></a>'
+            var gram = $scope.$parent.item.data;
+            $scope.gram = gram;
+            return $scope.gram;
+        }
     };
 };
 
