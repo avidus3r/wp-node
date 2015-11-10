0bf870cebc6e43e35ec32db9391b61137f92f0af
Christiaan Mealey <christiaan.mealey@altdriver.com>
2015-11-10 09:38:55 -0500

    gtm altdriver id

--- app/directives/instagram.js
+++ app/directives/instagram.js
@@ -4,14 +4,13 @@ var instagram = function() {
     return {
         restrict: 'EA',
         controller: function($scope, $attrs, InstagramService) {
-            $scope.grams = [];
+            $scope.gram = {};
             var maxId = $attrs.maxId;
             return InstagramService.get(1, 'nofilter', maxId).then(function(res){
                 $scope.gram = res.data;
                 return $scope.gram;
             });
-        }
-        //, template: '<a href="{{ gram.data.link }}" target="_blank"><img class="instagram-img" src="{{ gram.data.images.standard_resolution.url }}"></a>'
+        }, template: '<a href="{{ gram.data.link }}" target="_blank"><img class="instagram-img" src="{{ gram.data.images.standard_resolution.url }}"></a>'
     };
 };
 
