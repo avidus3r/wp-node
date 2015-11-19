9ab856650fcf1655972321d185f65809caf09010
Christiaan Mealey <christiaan.mealey@altdriver.com>
2015-11-19 15:49:36 -0500

    port 3000

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var express     = require('express'),
     authorized  = false,
     md5         = require('js-md5');
 
-var EXPRESS_PORT = 3001,
+var EXPRESS_PORT = 3000,
     EXPRESS_HOST = '127.0.0.1',
     EXPRESS_ROOT = './dist',
     feedConfig = null;
