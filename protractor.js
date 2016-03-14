547fa2c2283f77368ac358744400331480073db2
Mark Tucker <marktucker@Marks-Mac-mini.local>
2016-03-14 15:00:59 -0400

    adding changes for categories-spec test

--- protractor.js
+++ protractor.js
@@ -1,4 +1,4 @@
 exports.config = {
     seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
-    specs: ['dist/tests/protractor/single-spec.js'/*'dist/tests/protractor/homepage-spec.js','dist/tests/protractor/categories-spec.js'*/]
+    specs: ['dist/tests/protractor/single-spec.js','dist/tests/protractor/homepage-spec.js','dist/tests/protractor/categories-spec.js']
 };
--- public/tests/protractor/categories-spec.js
+++ public/tests/protractor/categories-spec.js
@@ -52,127 +52,142 @@ describe('categories', function() {
                     // feed should have at least 1 item
                     expect(feedItems.count()).toBeGreaterThan(0);
                     expect(browser.window.innerHeight).toBeGreaterThan(0);
-                    browser.executeScript('window.scrollTo(0,' + browser.window.innerHeight + ');').then(function () {
 
-                    });
                     menuButton.click();
                     menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
                     expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
+                    var currentUrl;
+                    var newUrl;
+                    browser.getCurrentUrl().then(function(url) {
+                            currentUrl = url;
+                        }
+                    ).then(function() {
+                        browser.executeScript('window.scrollTo(0,3000);');
+                    }).then(function () {
+                        browser.getCurrentUrl().then(function(url) {
+                            newUrl = url;
+                        }).then(function () {
+                            expect(currentUrl !== newUrl, 'page url changes');
+                            var newfeedItems = element.all(by.css('.feed-item'));
+                            expect(newfeedItems).toBeGreaterThan(feedItems);
+                        })
+                    })
+
                 });
             });
-            // menu button click should open nav
-            menuButton.click();
-            expect(array[1] === 'CRASH & BURN', 'menu item 0 expected to be CRASH & BURN');
-            element.all(by.repeater('item in navItems')).get(1).element(by.linkText('CRASH & BURN')).click().then(function () {
-                it('should have feed items', function () {
-                    browser.get('http://local.altdriver.com:3000/crash-burn/');
-
-                    //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-                    var feedItems = element.all(by.css('.feed-item'));
-
-                    // feed should have at least 1 item
-                    expect(feedItems.count()).toBeGreaterThan(0);
-                });
-                menuButton.click();
-                menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
-                expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
-            });
-            menuButton.click();
-            expect(array[2] === 'GAMING', 'menu item 0 expected to be GAMING');
-            element.all(by.repeater('item in navItems')).get(2).element(by.linkText('GAMING')).click().then(function() {
-                it('should have feed items', function () {
-                    browser.get('http://local.altdriver.com:3000/gaming/');
-
-                    //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-                    var feedItems = element.all(by.css('.feed-item'));
-
-                    // feed should have at least 1 item
-                    expect(feedItems.count()).toBeGreaterThan(0);
-                 });
-                menuButton.click();
-                menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
-                expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
-            });
-            menuButton.click();
-            expect(array[3] === 'SPORTS CAR', 'menu item 0 expected to be SPORTS CAR');
-            element.all(by.repeater('item in navItems')).get(3).element(by.linkText('SPORTS CAR')).click().then(function() {
-                it('should have feed items', function() {
-                    browser.get('http://local.altdriver.com:3000/gaming/');
-
-                    //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-                    var feedItems = element.all(by.css('.feed-item'));
-
-                    // feed should have at least 1 item
-                    expect(feedItems.count()).toBeGreaterThan(0);
-                });
-                menuButton.click();
-                menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
-                expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
-            });
-            menuButton.click();
-            expect(array[4] === 'LUX', 'menu item 0 expected to be LUX');
-            element.all(by.repeater('item in navItems')).get(4).element(by.linkText('LUX')).click().then(function() {
-                it('should have feed items', function() {
-                    browser.get('http://local.altdriver.com:3000/luxury/');
-
-                    //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-                    var feedItems = element.all(by.css('.feed-item'));
-
-                    // feed should have at least 1 item
-                    expect(feedItems.count()).toBeGreaterThan(0);
-                });
-                menuButton.click();
-                menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
-                expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
-            });
-            menuButton.click();
-            expect(array[5] === 'MOTORCYCLES', 'menu item 0 expected to be MOTORCYCLES');
-            element.all(by.repeater('item in navItems')).get(5).element(by.linkText('MOTORCYCLES')).click().then(function() {
-                it('should have feed items', function() {
-                    browser.get('http://local.altdriver.com:3000/motorcycles/');
-
-                    //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-                    var feedItems = element.all(by.css('.feed-item'));
-
-                    // feed should have at least 1 item
-                    expect(feedItems.count()).toBeGreaterThan(0);
-                });
-                menuButton.click();
-                menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
-                expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
-            });
-            menuButton.click();
-            expect(array[6] === 'CLASSICS', 'menu item 0 expected to be CLASSICS');
-            element.all(by.repeater('item in navItems')).get(6).element(by.linkText('CLASSICS')).click().then(function() {
-                it('should have feed items', function() {
-                    browser.get('http://local.altdriver.com:3000/classics/');
-
-                    //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-                    var feedItems = element.all(by.css('.feed-item'));
-
-                    // feed should have at least 1 item
-                    expect(feedItems.count()).toBeGreaterThan(0);
-                });
-                menuButton.click();
-                menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
-                expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
-            });
-            menuButton.click();
-            expect(array[7] === 'OFF ROAD', 'menu item 0 expected to be OFF ROAD');
-            element.all(by.repeater('item in navItems')).get(7).element(by.linkText('OFF ROAD')).click().then(function() {
-                it('should have feed items', function() {
-                    browser.get('http://local.altdriver.com:3000/off-road/');
-
-                    //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-                    var feedItems = element.all(by.css('.feed-item'));
-
-                    // feed should have at least 1 item
-                    expect(feedItems.count()).toBeGreaterThan(0);
-                });
-                menuButton.click();
-                menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
-                expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
-            });
+            //// menu button click should open nav
+            //menuButton.click();
+            //expect(array[1] === 'CRASH & BURN', 'menu item 0 expected to be CRASH & BURN');
+            //element.all(by.repeater('item in navItems')).get(1).element(by.linkText('CRASH & BURN')).click().then(function () {
+            //    it('should have feed items', function () {
+            //        browser.get('http://local.altdriver.com:3000/crash-burn/');
+            //
+            //        //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+            //        var feedItems = element.all(by.css('.feed-item'));
+            //
+            //        // feed should have at least 1 item
+            //        expect(feedItems.count()).toBeGreaterThan(0);
+            //    });
+            //    menuButton.click();
+            //    menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
+            //    expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
+            //});
+            //menuButton.click();
+            //expect(array[2] === 'GAMING', 'menu item 0 expected to be GAMING');
+            //element.all(by.repeater('item in navItems')).get(2).element(by.linkText('GAMING')).click().then(function() {
+            //    it('should have feed items', function () {
+            //        browser.get('http://local.altdriver.com:3000/gaming/');
+            //
+            //        //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+            //        var feedItems = element.all(by.css('.feed-item'));
+            //
+            //        // feed should have at least 1 item
+            //        expect(feedItems.count()).toBeGreaterThan(0);
+            //     });
+            //    menuButton.click();
+            //    menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
+            //    expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
+            //});
+            //menuButton.click();
+            //expect(array[3] === 'SPORTS CAR', 'menu item 0 expected to be SPORTS CAR');
+            //element.all(by.repeater('item in navItems')).get(3).element(by.linkText('SPORTS CAR')).click().then(function() {
+            //    it('should have feed items', function() {
+            //        browser.get('http://local.altdriver.com:3000/gaming/');
+            //
+            //        //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+            //        var feedItems = element.all(by.css('.feed-item'));
+            //
+            //        // feed should have at least 1 item
+            //        expect(feedItems.count()).toBeGreaterThan(0);
+            //    });
+            //    menuButton.click();
+            //    menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
+            //    expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
+            //});
+            //menuButton.click();
+            //expect(array[4] === 'LUX', 'menu item 0 expected to be LUX');
+            //element.all(by.repeater('item in navItems')).get(4).element(by.linkText('LUX')).click().then(function() {
+            //    it('should have feed items', function() {
+            //        browser.get('http://local.altdriver.com:3000/luxury/');
+            //
+            //        //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+            //        var feedItems = element.all(by.css('.feed-item'));
+            //
+            //        // feed should have at least 1 item
+            //        expect(feedItems.count()).toBeGreaterThan(0);
+            //    });
+            //    menuButton.click();
+            //    menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
+            //    expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
+            //});
+            //menuButton.click();
+            //expect(array[5] === 'MOTORCYCLES', 'menu item 0 expected to be MOTORCYCLES');
+            //element.all(by.repeater('item in navItems')).get(5).element(by.linkText('MOTORCYCLES')).click().then(function() {
+            //    it('should have feed items', function() {
+            //        browser.get('http://local.altdriver.com:3000/motorcycles/');
+            //
+            //        //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+            //        var feedItems = element.all(by.css('.feed-item'));
+            //
+            //        // feed should have at least 1 item
+            //        expect(feedItems.count()).toBeGreaterThan(0);
+            //    });
+            //    menuButton.click();
+            //    menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
+            //    expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
+            //});
+            //menuButton.click();
+            //expect(array[6] === 'CLASSICS', 'menu item 0 expected to be CLASSICS');
+            //element.all(by.repeater('item in navItems')).get(6).element(by.linkText('CLASSICS')).click().then(function() {
+            //    it('should have feed items', function() {
+            //        browser.get('http://local.altdriver.com:3000/classics/');
+            //
+            //        //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+            //        var feedItems = element.all(by.css('.feed-item'));
+            //
+            //        // feed should have at least 1 item
+            //        expect(feedItems.count()).toBeGreaterThan(0);
+            //    });
+            //    menuButton.click();
+            //    menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
+            //    expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
+            //});
+            //menuButton.click();
+            //expect(array[7] === 'OFF ROAD', 'menu item 0 expected to be OFF ROAD');
+            //element.all(by.repeater('item in navItems')).get(7).element(by.linkText('OFF ROAD')).click().then(function() {
+            //    it('should have feed items', function() {
+            //        browser.get('http://local.altdriver.com:3000/off-road/');
+            //
+            //        //element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+            //        var feedItems = element.all(by.css('.feed-item'));
+            //
+            //        // feed should have at least 1 item
+            //        expect(feedItems.count()).toBeGreaterThan(0);
+            //    });
+            //    menuButton.click();
+            //    menuState = menuButtonOpen.count() === 1 ? 'open' : 'closed';
+            //    expect(menuState).toEqual('closed', 'menu was open, button click expected to close menu');
+            //});
         });
 
     });
