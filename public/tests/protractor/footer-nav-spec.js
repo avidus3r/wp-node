3ae53e2b4c4a92f468b92a0286560fb6fcaa5c8b
Mark Tucker <marktucker@Marks-Mac-mini.local>
2016-04-14 16:16:23 -0400

    Added test for display of footer nav

--- public/tests/protractor/footer-nav-spec.js
+++ public/tests/protractor/footer-nav-spec.js
@@ -3,11 +3,15 @@ describe('footer-navigation', function() {
     var vistoragreementLink = element(by.css('.ga-nav-footer-visitor'));
     var privacyLink = element(by.css('.ga-nav-footer-privacy'));
 
+    it('mega-footer should be displayed', function() {
+        browser.get('http://local.altdriver.com:3000');
+        expect(element.all(by.css('.mega-footer')).isDisplayed()).toBeTruthy();
+
+    });
 
     //test contact menu link
     it('should navigate to contact view', function() {
         browser.ignoreSynchronization = true;
-        browser.get('http://local.altdriver.com:3000');
         contactLink.click();
         expect(browser.driver.getCurrentUrl()).toEqual("http://local.altdriver.com:3000/contact");
         browser.navigate().back();
