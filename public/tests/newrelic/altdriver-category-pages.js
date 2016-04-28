6589222bffc9b7807d9bbba6fe61209f535c4998
Mark Tucker <marktucker@Marks-Mac-mini.local>
2016-04-28 15:02:16 -0400

    Bug fix to correct nav closeanimation

--- public/tests/newrelic/altdriver-category-pages.js
+++ public/tests/newrelic/altdriver-category-pages.js
@@ -1,3 +1,20 @@
-/**
- * Created by marktucker on 4/28/16.
- */
+var assert = require('assert');
+$browser.addHostnameToWhitelist({hostnameArr: ['netdna-cdn.com']});
+
+$browser.get('http://www.altdriver.com').then(function(){
+    return $browser.waitForAndFindElement($driver.By.className("navbar-toggle"), 22000).then(function(element){
+        return element.click();
+    });
+})
+.then(function(){
+    console.log('Clicking Classics');
+    return $browser.waitForAndFindElement($driver.By.linkText("CLASSICS")).then(function(element){
+        return element.click();
+    });
+})
+.then(function(){
+    console.log('feed items');
+    return $browser.findElements($driver.By.className("feed-item")).then(function(elements){
+        return assert.equal(true, elements.length > 1, "No featured videos");
+    });
+});
